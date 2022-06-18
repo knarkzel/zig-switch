6aed43e8c01c41adf10b5042486ba7b1ba47db67
Knarkzel <knarkzel@gmail.com>
2022-06-18 19:28:04 +0200

    Ryujinx > Yuzu

--- build.zig
+++ build.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
+const emulator = "Ryujinx";
+const flags = .{"-lnx"};
 const devkitpro = "/opt/devkitpro";
 
 pub fn build(b: *std.build.Builder) void {
@@ -14,34 +17,30 @@ pub fn build(b: *std.build.Builder) void {
     obj.setTarget(.{
         .cpu_arch = .aarch64,
         .os_tag = .freestanding,
-        .abi = .eabi,
         .cpu_model = .{ .explicit = &std.Target.aarch64.cpu.cortex_a57 },
     });
     obj.setBuildMode(mode);
 
-    const elf = b.addSystemCommand(&.{
-        devkitpro ++ "/devkitA64/bin/aarch64-none-elf-gcc",
-        "-specs=" ++ devkitpro ++ "/libnx/switch.specs",
+    const extension = if (builtin.target.os.tag == .windows) ".exe" else "";
+    const elf = b.addSystemCommand(&(.{
+        devkitpro ++ "/devkitA64/bin/aarch64-none-elf-gcc" ++ extension,
         "-g",
         "-march=armv8-a+crc+crypto",
         "-mtune=cortex-a57",
         "-mtp=soft",
         "-fPIE",
         "-Wl,-Map,zig-out/zig-switch.map",
+        "-specs=" ++ devkitpro ++ "/libnx/switch.specs",
         "zig-out/zig-switch.o",
         "-L" ++ devkitpro ++ "/libnx/lib",
         "-L" ++ devkitpro ++ "/portlibs/switch/lib",
-        "-lglad",
-        "-lEGL",
-        "-lglapi",
-        "-ldrm_nouveau",
-        "-lnx",
+    } ++ flags ++ .{
         "-o",
         "zig-out/zig-switch.elf",
-    });
+    }));
 
     const nro = b.addSystemCommand(&.{
-        devkitpro ++ "/tools/bin/elf2nro",
+        devkitpro ++ "/tools/bin/elf2nro" ++ extension,
         "zig-out/zig-switch.elf",
         "zig-out/zig-switch.nro",
     });
@@ -49,4 +48,9 @@ pub fn build(b: *std.build.Builder) void {
     b.default_step.dependOn(&nro.step);
     nro.step.dependOn(&elf.step);
     elf.step.dependOn(&obj.step);
+
+    const run_step = b.step("run", "Run in Yuzu");
+    const yuzu = b.addSystemCommand(&.{ emulator, "zig-out/zig-switch.nro" });
+    run_step.dependOn(&nro.step);
+    run_step.dependOn(&yuzu.step);
 }
--- src/c.zig
+++
@@ -1,7 +0,0 @@
-pub usingnamespace @cImport({
-    @cDefine("__SWITCH__", {});
-    @cInclude("switch.h");
-    @cInclude("EGL/egl.h");
-    @cInclude("EGL/eglext.h");
-    @cInclude("glad/glad.h");
-});
